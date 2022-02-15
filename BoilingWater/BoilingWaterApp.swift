a544e576e6864ac8b2edbd6a7933c3314b6b9203
Nicole Huang <nhuang@lcs.on.ca>
2022-02-15 11:40:29 -0500

    Initial Commit

---
+++ BoilingWater/BoilingWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoilingWaterApp.swift
+//  BoilingWater
+//
+//  Created by Nicole on 2022/2/15.
+//
+
+import SwiftUI
+
+@main
+struct BoilingWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoilingWater/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoilingWater
+//
+//  Created by Nicole on 2022/2/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
